c58e43829c3f838127008827bba5ac76f8ce471a
Jeffrey Desir <jeffreyjdesir@gmail.com>
2020-01-07 13:25:41 -0500

    fixed route issue

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,24 +1,20 @@
-
 /*
-    Date: 1/6/2020
-    Author: Ashley Dunam
-*/
+ Date: 01.07.2020
+ Author: Ashley Dunham
+ */
 
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 function PrivateRoute(props) {
-
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-
-function PrivateRoute(props){
+    const {
+        component: Component,
+        ...rest
+    } = props
 
     return (
         <Route  {...rest} render={(renderProps) => {
-
-            if (localStorage.getItem("token")){
-
+            if (localStorage.getItem("token")) {
                 return <Component {...renderProps} />
             } else {
                 return <Redirect to="/" />
