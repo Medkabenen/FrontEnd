50d525c57d15f74ec66bf90a745a725a34ff6961
Ashley Dunham <xenocide21@gmail.com>
2020-01-07 18:38:28 -0700

    search form submits data and receives a response (pending response data being displayed and manipulated into state management)

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,35 @@
-import react from "react";
-import { Input } from "antd";
+import React, {useState} from "react";
+// import { Input } from "antd";
+// import { api } from '../utils/AxiosWithAuth'
+import axios from 'axios'
 
 import "antd/dist/antd.css";
 
-const Search = (source) => {
-    return (
-        <Input>
 
-        </Input>
+
+export default function Search () {
+  const [data, setData] = useState({})
+
+  const handleChange = e =>{
+    e.preventDefault()
+    setData({...data, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+      .post('https://cors-anywhere.herokuapp.com/https://medcab3-api.herokuapp.com/strains', data)
+      .then(r => { console.log(r) })
+      .catch(err => { console.log(err) })
+  }
+    return (
+      <form className='searchForm' onSubmit={handleSubmit}>
+        Name: <input type='text'
+                     name='input'
+                     value={data.input}
+                     onChange={handleChange}
+      />
+        <button type='submit'>Search Recommended Strains</button>
+      </form>
     )
 }
-
-export default Search;
