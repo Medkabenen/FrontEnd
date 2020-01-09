baea0c7bf3e3b28c22f890ac15e889ea96915c5b
Jeffrey Desir <jeffreyjdesir@gmail.com>
2020-01-08 19:46:02 -0500

    rebase on ad.745

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,68 +5,67 @@ import { Form, Icon, Input, Button, Checkbox } from "antd";
 import "antd/dist/antd.css"
 
 const emptyForm = {
-  username: '',
-  password: ''
+    username: '',
+    password: ''
 }
 
 const Login = (props) => {
-  const [info, setInfo] = useState(emptyForm)
+    const [info, setInfo] = useState(emptyForm)
 
-  const handleChange = e => {
-    setInfo({
-      ...info,
-      [e.target.name]: e.target.value
-    })
-  }
+    const handleChange = e => {
+        setInfo({
+            ...info,
+            [e.target.name]: e.target.value
+        })
+    }
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    axiosWithAuth().post('api/login', info)
-      .then(res => {
-        localStorage.setItem('token', res.data.payload);
-        setInfo(emptyForm)
-        props.history.push('/account')
-      })
-      .catch(error => console.log(error, "Login Error"))
-  }
+    const handleSubmit = e => {
+        e.preventDefault();
+        axiosWithAuth().post('api/login', info)
+            .then(res => {
+                localStorage.setItem('token', res.data.payload);
+                setInfo(emptyForm)
+                props.history.push('/account')
+            })
+            .catch(error => console.log(error, "Login Error"))
+    }
 
-  return (
-    <Form onSubmit={handleSubmit} className="login-form">
-      <Form.Item>
-        <Input
-          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder="Username"
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={info.username}
-          onChange={handleChange}
-        />,
-      )}
-        </Form.Item>
+    return (
+        <Form onSubmit={handleSubmit} className="login-form">
+            <Form.Item>
+                <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Username"
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    value={info.username}
+                    onChange={handleChange}
+                />
+            </Form.Item>
 
-      <Form.Item>
-        <Input
-          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          type="password"
-          placeholder="Password"
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={info.password}
-          onChange={handleChange}
-        />,
-      )}
-      </Form.Item>
+            <Form.Item>
+                <Input
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    type="password"
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={info.password}
+                    onChange={handleChange}
+                />
+            </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button" className="button" type="submit">
-          Log in
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button" className="button" type="submit">
+                    Log in
           </Button>
-      </Form.Item>
+            </Form.Item>
 
-    </Form>
-  )
+        </Form>
+    )
+
 };
 
 export default Login;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-// import CounterContainer from '../containers/CounterContainer'
-import Header from '../components/Header'
 import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import styled from '@emotion/styled'
 import Login from "../components/Login";
 import PrivateRoute from "../utils/PrivateRoute";
-import account from "../components/Account"
+import Dashboard from "../components/Dashboard/index";
 
 const Container = styled.div`
   text-align: center;
@@ -16,14 +14,15 @@ export const history = createBrowserHistory()
 function Routes() {
   return (
     <Router history={history}>
-      <Container>
-        <Header />
-        <Switch>
-          {/*<Route path="/" component={CounterContainer} />*/}
-          <Route exact path="/" component={Login} />
-          <PrivateRoute exact path="/account" component={account} />
-        </Switch>
-      </Container>
+
+
+      <Switch>
+        <Route path="/" component={Dashboard} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route exact path="/" component={Login} />
+        <PrivateRoute exact path="/account" component={Dashboard} />
+      </Switch>
+
     </Router>
   )
 }
