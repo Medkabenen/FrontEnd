193ba3811b4b614bcaee6ff23eb7841f7e858ef9
Ashley Dunham <xenocide21@gmail.com>
2020-01-09 10:06:32 -0800

    edited search criteria and search options (search by flavor/effects or search by symptoms)

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Search from "./Search";
+
 
 export default class Account extends Component {
 
   render() {
     return (
       <div>
-        <p>Test</p>
+        <Search/>
+        <a href='https://potency-calc.herokuapp.com/predictions'>Potency Calculator</a>
       </div>
     )
   }
