bc23d22dbe125aaf7ba9241c6897bad2f395cff2
Jeffrey Desir <jeffreyjdesir@gmail.com>
2020-01-08 19:38:16 -0500

    w

---
+++ src/components/Dashboard/NavMenu.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Layout, Icon } from 'antd';
+import { Menu } from "antd";
+
+const { SubMenu } = Menu;
+
+const menuStyle = {
+    overflow: 'auto',
+    height: '100vh',
+    position: 'fixed',
+    left: 0,
+}
+const NavMenu = _ => {
+
+    return (
+
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1">
+                <Icon type="home" />
+                <span className="nav-text">Home</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+                <Icon type="video-camera" />
+                <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+                <Icon type="upload" />
+                <span className="nav-text">nav 3</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+                <Icon type="bar-chart" />
+                <span className="nav-text">nav 4</span>
+            </Menu.Item>
+            <Menu.Item key="5">
+                <Icon type="cloud-o" />
+                <span className="nav-text">nav 5</span>
+            </Menu.Item>
+            <Menu.Item key="6">
+                <Icon type="appstore-o" />
+                <span className="nav-text">nav 6</span>
+            </Menu.Item>
+            <Menu.Item key="7">
+                <Icon type="team" />
+                <span className="nav-text">nav 7</span>
+            </Menu.Item>
+            <Menu.Item key="8">
+                <Icon type="shop" />
+                <span className="nav-text">nav 8</span>
+            </Menu.Item>
+        </Menu>
+    )
+}
+
+export default NavMenu;
